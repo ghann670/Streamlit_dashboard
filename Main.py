a80f57cab46566b9fe1e6155400b0a4cf30370dc
Gyuhyun Ann <gyuhyun@jang-yuntaeui-MacBookPro.local>
2025-07-21 07:11:10 +0900

    Remove debugging code and restore original metrics

--- Main.py
+++ Main.py
@@ -482,14 +482,8 @@ left_col, right_col = st.columns([3, 2])  # 히스토그램이 더 넓게
 
 with left_col:
     # earnings/briefing 사용자 수 표시
-    st.write("Debug - Unique earnings values:", df_all['earnings'].unique())
-    st.write("Debug - Unique briefing values:", df_all['briefing'].unique())
-    
     earnings_users = len(df_all[df_all['earnings'] == 'onboarded']['user_email'].unique())
     briefing_users = len(df_all[df_all['briefing'] == 'onboarded']['user_email'].unique())
-    
-    st.write("Debug - earnings_users:", earnings_users)
-    st.write("Debug - briefing_users:", briefing_users)
     
     col1, col2 = st.columns(2)
     with col1:
